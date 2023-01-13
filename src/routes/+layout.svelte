d2716f5add266fa1a52a61c41f9c3bc92fb684b2
Luke Chadwick <luke@vertis.io>
2023-01-13 10:48:51 +0000

    feat: demonstrate user is logged in

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,23 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+  import { LayoutLoad, LayoutData } from './$types';
+  // Load the user's session
+  export function load({ cookies }: LayoutLoad) {
+    const userCookie = cookies.user;
+    if (!userCookie) {
+      return {};
+    }
+  return {
+    user: JSON.parse(userCookie) 
+  };
+}
+export let data: LayoutData;
 </script>
+
+{#if data.user}
+  <p>Welcome back, {data.user.name}!</p>
+{:else}
+  <p>Not logged in</p>
+{/if}
 
 <slot />
